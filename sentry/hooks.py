871a3929d05f4f12a4b6aafe1a4478d240a72c45
Sun Howwrongbum <sun@libermatic.com>
2021-01-22 01:20:03 +0000

    feat: setup  sentry

--- sentry/hooks.py
+++ sentry/hooks.py
@@ -17,7 +17,7 @@ app_license = "MIT"
 
 # include js, css files in header of desk.html
 # app_include_css = "/assets/sentry/css/sentry.css"
-# app_include_js = "/assets/sentry/js/sentry.js"
+app_include_js = ["/assets/js/sentry.min.js"]
 
 # include js, css files in header of web template
 # web_include_css = "/assets/sentry/css/sentry.css"
---
+++ sentry/public/js/index.js
@@ -0,0 +1,40 @@
+import * as Sentry from '@sentry/browser';
+import { Integrations } from '@sentry/tracing';
+import * as R from 'ramda';
+
+const dsn =
+  !frappe.boot.developer_mode && frappe.boot.sentry
+    ? frappe.boot.sentry.dsn
+    : null;
+
+const getErrorName = R.compose(R.last, R.dropLast(1), R.split('\\n'), R.trim);
+
+if (dsn) {
+  Sentry.init({
+    dsn,
+    release: `frappe@${frappe.boot.versions.frappe}`,
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+  Sentry.setUser({ email: frappe.boot.user.email });
+  Sentry.setTag('sitename', frappe.boot.sitename);
+  Sentry.setContext('apps', frappe.boot.versions);
+
+  $(document).ajaxError(function (_event, jqXHR, ajaxSettings, thrownError) {
+    const message =
+      (jqXHR.responseJSON &&
+        jqXHR.responseJSON.exc &&
+        getErrorName(jqXHR.responseJSON.exc)) ||
+      '';
+
+    Sentry.captureMessage(thrownError || jqXHR.statusText, (scope) => {
+      scope.setExtras({
+        method: ajaxSettings.type,
+        url: ajaxSettings.url,
+        payload: ajaxSettings.data,
+        status: jqXHR.status,
+        message,
+      });
+    });
+  });
+}
