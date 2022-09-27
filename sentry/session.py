192bee34d30512690faa45baa9dfe36a7aeae4a3
Sun Howwrongbum <sun@libermatic.com>
2022-09-27 01:21:17 +0000

    feat: get build version from other src

--- sentry/session.py
+++ sentry/session.py
@@ -1,15 +1,25 @@
+import os
 import frappe
 from frappe.utils import get_build_version
 
 
 def extend_bootinfo(bootinfo):
     if frappe.get_cached_value("Sentry Settings", None, "enable_sentry"):
-        buildtime = frappe.utils.datetime.datetime.fromtimestamp(
-            frappe.utils.cint(get_build_version())
-        )
         bootinfo.sentry = {
             "dsn": frappe.conf.get("sentry_dsn"),
-            "build_version": "{}.{}.{}".format(
-                buildtime.year % 100, buildtime.month, buildtime.day
-            ),
+            "build_version": _get_build_version(),
         }
+
+
+def _get_build_version():
+    if frappe.conf.developer_mode:
+        return "dev"
+
+    build_version = os.getenv("BUILD_VERSION")
+    if build_version:
+        return build_version
+
+    buildtime = frappe.utils.datetime.datetime.fromtimestamp(
+        frappe.utils.cint(get_build_version())
+    )
+    return "{}.{}.{}".format(buildtime.year % 100, buildtime.month, buildtime.day)
